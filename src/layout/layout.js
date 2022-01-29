593c54582bd851886f38f57e51d62cfcd1f5ff4b
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2022-01-29 19:36:20 +0100

    Dodanie strony 404.

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import './layout.scss';
 
 export const Layout = ({children}) => {
     return (
-        <div>
+        <div className={'layout'}>
             <main>
                 {children}
             </main>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,21 @@
 import * as React from 'react';
+import {ButtonLink} from "../fragments/button-link";
 
 const NotFoundPage = () => {
     return (
-        <main/>
+        <main className={'container'}>
+            <div className={'row min-vh-100 align-items-center'}>
+                <div className={'col text-center'}>
+                    <h1 className={'display-1 fw-lighter'}>404</h1>
+                    <h2 className={'h5 mt-3'}>Strona nie została znaleziona</h2>
+                    <p className={'mt-3 mb-5'}>Strona której szukasz nie istnieje lub wystąpił inny błąd.</p>
+
+                    <ButtonLink
+                        href={'/'}
+                        label={'Wróć do strony głównej'}/>
+                </div>
+            </div>
+        </main>
     )
 }
 
