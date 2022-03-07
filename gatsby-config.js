ae53ea41ae2e1af21722dafa1b247dddd9fbbcf4
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2022-03-07 20:31:21 +0100

    Dodanie identyfikatora śledzenia.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "XXX",
+        trackingId: "G-RMHF103NDQ",
       },
     },
     {
